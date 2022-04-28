b51c454ac89267c7bfb781ce58615162b81f8edd
ViktorsLV <viktors.ch11@gmail.com>
2022-04-28 16:48:00 +0200

    fix mistake

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -8,7 +8,7 @@ describe("Header", () => {
               title="todo"
             />
         );
-        const h1Element = screen.getByText(/xdd/i);
+        const h1Element = screen.getByText(/todo/i);
         expect(h1Element).toBeInTheDocument();
     });
 })
