73f98d2b88e39c23e11f76885288cb3e1b87c8d2
ViktorsLV <viktors.ch11@gmail.com>
2022-05-26 17:07:18 +0200

    adding docker actions to workflows

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Blogs from './components/Blogs';
 
 function App() {
   return (
     <div className="App">
       <Header title={'yellow world!'}/>
+      <Blogs/>
     </div>
   );
 }
